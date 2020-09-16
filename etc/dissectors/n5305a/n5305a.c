3385a972c07ee6d1811695cb6ea2c8239c036e44
dx-mon <dx-mon@users.sourceforge.net>
2020-09-16 02:58:27 +0100

    Fixed the flags values not displaying properly

--- etc/dissectors/n5305a/n5305a.c
+++ etc/dissectors/n5305a/n5305a.c
@@ -49,112 +49,112 @@ static hf_register_info fields[] =
 		&flags[0],
 		{
 			"Flag 0", "n5305a.protocol_analyzer.flags.flag0",
-			FT_BOOLEAN, 1, NULL, 0x0001, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x0001, NULL, HFILL
 		}
 	},
 	{
 		&flags[1],
 		{
 			"Flag 1", "n5305a.protocol_analyzer.flags.flag1",
-			FT_BOOLEAN, 1, NULL, 0x0002, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x0002, NULL, HFILL
 		}
 	},
 	{
 		&flags[2],
 		{
 			"Flag 2", "n5305a.protocol_analyzer.flags.flag2",
-			FT_BOOLEAN, 1, NULL, 0x0004, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x0004, NULL, HFILL
 		}
 	},
 	{
 		&flags[3],
 		{
 			"Flag 3", "n5305a.protocol_analyzer.flags.flag3",
-			FT_BOOLEAN, 1, NULL, 0x0008, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x0008, NULL, HFILL
 		}
 	},
 	{
 		&flags[4],
 		{
 			"Flag 4", "n5305a.protocol_analyzer.flags.flag4",
-			FT_BOOLEAN, 1, NULL, 0x0010, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x0010, NULL, HFILL
 		}
 	},
 	{
 		&flags[5],
 		{
 			"Flag 5", "n5305a.protocol_analyzer.flags.flag5",
-			FT_BOOLEAN, 1, NULL, 0x0020, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x0020, NULL, HFILL
 		}
 	},
 	{
 		&flags[6],
 		{
 			"Flag 6", "n5305a.protocol_analyzer.flags.flag6",
-			FT_BOOLEAN, 1, NULL, 0x0040, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x0040, NULL, HFILL
 		}
 	},
 	{
 		&flags[7],
 		{
 			"Flag 7", "n5305a.protocol_analyzer.flags.flag7",
-			FT_BOOLEAN, 1, NULL, 0x0080, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x0080, NULL, HFILL
 		}
 	},
 	{
 		&flags[8],
 		{
 			"Flag 8", "n5305a.protocol_analyzer.flags.flag8",
-			FT_BOOLEAN, 1, NULL, 0x0100, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x0100, NULL, HFILL
 		}
 	},
 	{
 		&flags[9],
 		{
 			"Flag 9", "n5305a.protocol_analyzer.flags.flag9",
-			FT_BOOLEAN, 1, NULL, 0x0200, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x0200, NULL, HFILL
 		}
 	},
 	{
 		&flags[10],
 		{
 			"Flag A", "n5305a.protocol_analyzer.flags.flaga",
-			FT_BOOLEAN, 1, NULL, 0x0400, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x0400, NULL, HFILL
 		}
 	},
 	{
 		&flags[11],
 		{
 			"Flag B", "n5305a.protocol_analyzer.flags.flagb",
-			FT_BOOLEAN, 1, NULL, 0x0800, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x0800, NULL, HFILL
 		}
 	},
 	{
 		&flags[12],
 		{
 			"Flag C", "n5305a.protocol_analyzer.flags.flagc",
-			FT_BOOLEAN, 1, NULL, 0x1000, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x1000, NULL, HFILL
 		}
 	},
 	{
 		&flags[13],
 		{
 			"Flag D", "n5305a.protocol_analyzer.flags.flagd",
-			FT_BOOLEAN, 1, NULL, 0x2000, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x2000, NULL, HFILL
 		}
 	},
 	{
 		&flags[14],
 		{
 			"Flag E", "n5305a.protocol_analyzer.flags.flage",
-			FT_BOOLEAN, 1, NULL, 0x4000, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x4000, NULL, HFILL
 		}
 	},
 	{
 		&flags[15],
 		{
 			"Transaction Complete", "n5305a.protocol_analyzer.flags.transaction_complete",
-			FT_BOOLEAN, 1, NULL, 0x8000, NULL, HFILL
+			FT_BOOLEAN, 16, NULL, 0x8000, NULL, HFILL
 		}
 	},
 	{
