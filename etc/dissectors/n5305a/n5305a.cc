b99553024d05fd7ff089b25a1e6ebeddde30dc08
Aki Van Ness <aki@lethalbit.net>
2020-09-18 18:18:22 -0400

    Fixed the dissector plugin version so it now showes up in wireshark properly

--- etc/dissectors/n5305a/n5305a.cc
+++ etc/dissectors/n5305a/n5305a.cc
@@ -4,13 +4,13 @@
 
 extern "C"
 {
-	extern const char *const plugin_version WS_DLL_PUBLIC_DEF;
+	extern const char plugin_version[] WS_DLL_PUBLIC_DEF;
 	extern const int plugin_want_major WS_DLL_PUBLIC_DEF;
 	extern const int plugin_want_minor WS_DLL_PUBLIC_DEF;
 	WS_DLL_PUBLIC void plugin_register();
 }
 
-const char *const plugin_version = "0.0.2";
+const char plugin_version[] = "0.0.2";
 const int plugin_want_major = WIRESHARK_VERSION_MAJOR;
 const int plugin_want_minor = WIRESHARK_VERSION_MINOR;
 
