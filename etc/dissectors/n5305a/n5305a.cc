b1c666897148b964fd1b0500864fc35ea474e43c
Aki Van Ness <aki@lethalbit.net>
2020-09-19 10:41:48 -0400

    Went on a bit of a commenting rampage

--- etc/dissectors/n5305a/n5305a.cc
+++ etc/dissectors/n5305a/n5305a.cc
@@ -14,15 +14,20 @@ const char plugin_version[] = "0.0.2";
 const int plugin_want_major = WIRESHARK_VERSION_MAJOR;
 const int plugin_want_minor = WIRESHARK_VERSION_MINOR;
 
+/* register the native wireshark plugin */
 void plugin_register()
 {
+	/* define the internal plugins for the frame and transaction dissectors */
 	static proto_plugin framePlugin;
 	static proto_plugin transactionPlugin;
 
+	/* Set the appropriate entry points for the frame dissectors  */
 	framePlugin.register_protoinfo = registerProtocolN5305AFraming;
 	framePlugin.register_handoff = registerDissectorN5305AFraming;
+	/* Register the plugin with wireshark */
 	proto_register_plugin(&framePlugin);
 
+	/* Same as above but for the transaction dissector */
 	transactionPlugin.register_protoinfo = registerProtocolN5305ATransaction;
 	transactionPlugin.register_handoff = registerDissectorN5305ATransaction;
 	proto_register_plugin(&transactionPlugin);
