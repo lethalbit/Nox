e82d5ba3c319beb9abb7bede2ad8b0faf11eb337
Aki Van Ness <aki@lethalbit.net>
2020-11-20 16:06:03 -0500

    Some initial prep for dynamically loadable modules for dissection ala wireshark

---
+++ src/libnox/include/core/module.hh
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+/* core/module.hh - Nox plugin / native module interface */
+#pragma once
+#if !defined(LIBNOX_CORE_MODULE_HH)
+#define LIBNOX_CORE_MODULE_HH
+
+#endif /* LIBNOX_CORE_MODULE_HH */
---
+++ src/libnox/include/core/module_loader.hh
@@ -0,0 +1,8 @@
+#pragma
+// SPDX-License-Identifier: LGPL-3.0-or-later
+/* core/module_loader.hh - Nox plugin / native module loader */
+#pragma once
+#if !defined(LIBNOX_CORE_MODULE_LOADER_HH)
+#define LIBNOX_CORE_MODULE_LOADER_HH
+
+#endif /* LIBNOX_CORE_MODULE_LOADER_HH */
