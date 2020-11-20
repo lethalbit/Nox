3970949de71e46b1252c4ce608a530750e66dc46
Aki Van Ness <aki@lethalbit.net>
2020-11-20 17:00:09 -0500

    Added some empty template stuff for i18n

--- src/nox-gui/i18n/en_US.ts
+++ src/nox-gui/i18n/en_US.ts
@@ -1,5 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
-
+	<context>
+		<name></name>
+		<message>
+			<location filename="" />
+			<source></source>
+			<translation></translation>
+		</message>
+	</context>
 </TS>
